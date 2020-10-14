cab9c47853b11ad4e9c3a67aba3000535ecf5f5f
Lint Action <lint-action@samuelmeuli.com>
2020-10-14 03:11:36 +0000

    Fix code style issues with Black

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,24 +1,30 @@
 import pytest
 
+
 @pytest.fixture
 def simple_fixture():
-    print('hi')
+    print("hi")
+
 
 def test_sample_function(simple_fixture):
     assert True
 
+
 class Paul:
     pass
+
+
 class Karen:
     pass
+
+
 class Paco:
     pass
+
+
 class Jennifer:
     pass
+
+
 class Mom:
     pass
-
-
-
-
-
