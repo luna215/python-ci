567b0634c8f7b8aadb6660cd5eeafc50b72adc63
luna215 <paulluna0215@gmail.com>
2021-03-13 11:26:45 -0800

    removing files

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -4,9 +4,9 @@ import pytest
 @pytest.fixture                 
 def simple_fixture():
     print("hi")
+
 def test_sample_function(simple_fixture):                       
     assert True
-
 
 class Paul:                     
     pass
