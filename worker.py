d23c13037c66c7403b32ca374be8cec760e7fd41
luna215 <paulluna0215@gmail.com>
2021-03-21 14:33:08 -0700

    added python.d to parse loggging from file

--- worker.py
+++ worker.py
@@ -3,5 +3,5 @@ import os
 if __name__ == '__main__':
     os.system('pipenv install')
     os.system('pipenv install --dev')
-    os.system('python main.py')
+    # os.system('python main.py')
     os.system('python another_file.py')
