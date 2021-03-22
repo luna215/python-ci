6fc508a677eb81b38091e4607c3f83f809ebac1e
luna215 <paulluna0215@gmail.com>
2021-03-22 15:01:43 -0700

    workinggit status

--- another_file.py
+++ another_file.py
@@ -10,19 +10,16 @@ logging.config.fileConfig(os.path.abspath('logging.ini'), disable_existing_logge
 logger = logging.getLogger(__name__)
 
 def record_word_count(myfile):
-    for i in range(100000):
-        logger.info("starting the function")
-    # try:
-    #     word_count = main.word_count(myfile)
-    #     with open('wordcountarchive.csv', 'a') as file:
-    #         row = str(myfile) + ',' + str(word_count)
-    #         file.write(row + '\n')
-    # except:
-    #     logger.warning("could not write file %s to destination", myfile)
-    # finally:
-    #     logger.debug("the function is done for the file %s", myfile)
+    try:
+        word_count = main.word_count(myfile)
+        with open('wordcountarchive.csv', 'a') as file:
+            row = str(myfile) + ',' + str(word_count)
+            file.write(row + '\n')
+    except:
+        logger.warning("could not write file %s to destination", myfile)
+    finally:
+        logger.debug("the function is done for the file %s", myfile)
 
 
 if __name__ == '__main__':
     record_word_count('doesnotexist.txt')
-    os.system('agent-wrapper status')
