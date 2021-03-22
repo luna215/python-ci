fe13a521ec2b4324b866125ff4025462d492f6f9
luna215 <paulluna0215@gmail.com>
2021-03-22 14:17:35 -0700

    hi

--- datadog/prerun.sh
+++ datadog/prerun.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-echo "ENABLING LOGS...."
-echo "datadog-agent status"
 datadog-agent status
-echo "logs_enabled: true" >> /app/.apt/etc/datadog-agent/datadog.yaml
+# echo "logs_enabled: true" >> /app/.apt/etc/datadog-agent/datadog.yaml
