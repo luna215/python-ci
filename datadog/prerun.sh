b3dd2f69f236986ad0b77a072b14c95319b1c991
luna215 <paulluna0215@gmail.com>
2021-03-22 15:18:44 -0700

    lets try again

--- datadog/prerun.sh
+++ datadog/prerun.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
 mkdir /app/.apt/etc/datadog-agent/conf.d/custom_log_collect.d
 touch /app/.apt/etc/datadog-agent/conf.d/custom_log_collect.d/conf.yaml
 cat /app/datadog/conf.d/python.d/logs.yaml >> /app/.apt/etc/datadog-agent/conf.d/custom_log_collect.d/conf.yaml
-# echo "logs_enabled: true" >> /app/.apt/etc/datadog-agent/datadog.yaml
