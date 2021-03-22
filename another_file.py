24b6793ff790f11d182100f78a63005d574d8b2a
luna215 <paulluna0215@gmail.com>
2021-03-22 14:52:12 -0700

    forcing this

--- another_file.py
+++ another_file.py
@@ -9,19 +9,18 @@ import main
 logging.config.fileConfig(os.path.abspath('logging.ini'), disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
 
-os.system('pwd')
-
 def record_word_count(myfile):
-    logger.info("starting the function")
-    try:
-        word_count = main.word_count(myfile)
-        with open('wordcountarchive.csv', 'a') as file:
-            row = str(myfile) + ',' + str(word_count)
-            file.write(row + '\n')
-    except:
-        logger.warning("could not write file %s to destination", myfile)
-    finally:
-        logger.debug("the function is done for the file %s", myfile)
+    for i in range(100000):
+        logger.info("starting the function")
+    # try:
+    #     word_count = main.word_count(myfile)
+    #     with open('wordcountarchive.csv', 'a') as file:
+    #         row = str(myfile) + ',' + str(word_count)
+    #         file.write(row + '\n')
+    # except:
+    #     logger.warning("could not write file %s to destination", myfile)
+    # finally:
+    #     logger.debug("the function is done for the file %s", myfile)
 
 
 if __name__ == '__main__':
--- datadog/prerun.sh
+++ datadog/prerun.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+mkdir /etc/datadog-agent/conf.d/custom_log_collect.d
+touch /etc/datadog-agent/conf.d/custom_log_collect.d/conf.yaml
+cat /app/datadog/conf.d/python.d/logs.yaml >> /etc/datadog-agent/conf.d/custom_log_collect.d/conf.yaml
 # echo "logs_enabled: true" >> /app/.apt/etc/datadog-agent/datadog.yaml
