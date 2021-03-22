b9028f653771522f5077be8de05602812ae8b002
luna215 <paulluna0215@gmail.com>
2021-03-21 17:57:47 -0700

    updated worker.py

--- worker.py
+++ worker.py
@@ -1,7 +1,10 @@
 import os
 
 if __name__ == '__main__':
+    print('Running `pipenv install`')
     os.system('pipenv install')
+    print('Running `pipenv install --dev`')
     os.system('pipenv install --dev')
     # os.system('python main.py')
+    print('Running `python another_file.py`')
     os.system('python another_file.py')
