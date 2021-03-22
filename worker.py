de6ca0f38f12262a6a072a86880e4d8ea620a4ad
luna215 <paulluna0215@gmail.com>
2021-03-21 18:24:27 -0700

    removed running Pipenv

--- worker.py
+++ worker.py
@@ -1,10 +1,5 @@
 import os
 
 if __name__ == '__main__':
-    print('Running `pipenv install`')
-    os.system('pipenv install')
-    print('Running `pipenv install --dev`')
-    os.system('pipenv install --dev')
-    # os.system('python main.py')
     print('Running `python another_file.py`')
     os.system('pipenv run python another_file.py')
