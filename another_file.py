1a6aea37b18cd01053cc88891ec1d0fbe82f7a7b
Paul Luna <paulluna@pauls-mbp.lan>
2021-03-27 14:35:06 -0700

    updated log format to include the following: company, request_id, organization

--- another_file.py
+++ another_file.py
@@ -4,25 +4,50 @@ import logging.config
 import traceback
 import time
 
-class CustomAdapter(logging.LoggerAdapter):
-    def process(self, msg, kwargs):
-        my_context = kwargs.pop('slide', self.extra['slide'])
-        return '[%s] %s' % (my_context, msg), kwargs
+# class CustomAdapter(logging.LoggerAdapter):
+#     def process(self, msg, kwargs):
+#         my_context = kwargs.pop('slide', self.extra['slide'])
+#         return '[%s] %s' % (my_context, msg), kwargs
+
+class DatabookFilter(logging.Filter):
+    """
+    This is a filter which injects contextual information into the log
+    """
+    REQUEST_ID = os.environ['REQUEST_ID']
+    ENVIRONMENT = os.environ['ENVIRONMENT']
+    COMPANY = 'Databook'
+    ORGANIZATION = 'Accenture'
+
+    #... Among other things
+
+    def filter(self, record):
+
+        record.request_id = DatabookFilter.REQUEST_ID
+        record.environment = DatabookFilter.ENVIRONMENT
+        record.company = DatabookFilter.COMPANY
+        record.organization = DatabookFilter.ORGANIZATION
+
+        return True
 
 logging.config.fileConfig(os.path.abspath('logging.ini'), disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
-logger = CustomAdapter(logger, {'slide': None})
+
+# logger = CustomAdapter(logger, {'slide': None})
+
+databook_filter = DatabookFilter()
+logger.addFilter(databook_filter)
+
 
 def record_word_count(myfile):
-    for i in range(100000):
+    for i in range(10):
         starttime = time.time()
         try:
             raise ValueError('There was an issue')
         except:
             endtime = time.time()
             duration = endtime - starttime
-            logger.error("Deckbot crashed: %s", traceback.format_exc(), slide='ExecutiveSummary', extra={'run_duration': duration, "request_id": 123, "organization": "salesforce", 'targat_company': 456, 'user_id': 321, 'user_email': 'paul.luna+salesforce@trydatabook.com'})
-            logger.error("Deckbot crashed: %s", traceback.format_exc(), slide='SolutionsSlide')
+            logger.error("Deckbot crashed: %s", traceback.format_exc(), extra={'run_duration': duration, "request_id": 123, 'user_email': 'paul.luna+salesforce@trydatabook.com'})
+            logger.error("Deckbot crashed: %s", traceback.format_exc())
     
     raise Error("Exiting app")
 
